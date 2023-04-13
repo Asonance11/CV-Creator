9814129ab67af721ce2ab4d9fdca9f8448fc9c6c
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-13 12:34:26 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className="text-3xl text-green-600 font-bold text-center my-5">
+			Hello World
+		</div>
+	);
+};
+
+export default App;
