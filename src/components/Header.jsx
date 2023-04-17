8851d63ee0829efa0e6ce2539fd42a0a9bc63d22
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-17 12:22:51 +0100

    addition of fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Header = () => {
 	return (
 		<header className="text-center  p-4 bg-slate-950">
-			<h1 className=" text-5xl text-gray-200 font-bold">CV Creator</h1>
+			<h1 className=" text-5xl text-gray-200 font-bold font-sans">
+				CV Creator
+			</h1>
 		</header>
 	);
 };
