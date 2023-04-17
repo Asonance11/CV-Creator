f93594f2435f39ce2bd28a4e9a33eaa92e05b63c
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-17 12:23:58 +0100

    header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<header className="text-center  p-4 bg-slate-950">
+		<header className="text-center  p-4 bg-slate-900">
 			<h1 className=" text-5xl text-gray-200 font-bold font-sans">
 				CV Creator
 			</h1>
