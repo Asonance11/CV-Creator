fc92b0f78b3a289af83ea882eae4963b1d2d4ba7
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-17 13:31:20 +0100

    initializig the component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,35 +1,40 @@
 import React, { Component } from 'react';
+import CVForm from './CV-Form/CVForm';
 
 export default class Main extends Component {
 	constructor() {
 		super();
 
-		this.state = {
-			personalInfo: {
-				firstName: '',
-				lastName: '',
-				jobTitle: '',
-				email: '',
-				phoneNumber: '',
-				address: '',
-			},
-		};
+		// this.state = {
+		// 	personalInfo: {
+		// 		firstName: '',
+		// 		lastName: '',
+		// 		jobTitle: '',
+		// 		email: '',
+		// 		phoneNumber: '',
+		// 		address: '',
+		// 	},
+		// };
 
-		updatePersonalInfo = (e) => {
-			this.setState({
-				personalInfo: {
-					firstName: this.state.personalInfo.firstName,
-					lastName: this.state.personalInfo.lastName,
-					jobTitle: this.state.personalInfo.jobTitle,
-					email: this.state.personalInfo.email,
-					phoneNumber: this.state.personalInfo.phoneNumber,
-					address: this.state.personalInfo.address,
-				},
-			});
-		};
+		// updatePersonalInfo = (e) => {
+		// 	this.setState({
+		// 		personalInfo: {
+		// 			firstName: this.state.personalInfo.firstName,
+		// 			lastName: this.state.personalInfo.lastName,
+		// 			jobTitle: this.state.personalInfo.jobTitle,
+		// 			email: this.state.personalInfo.email,
+		// 			phoneNumber: this.state.personalInfo.phoneNumber,
+		// 			address: this.state.personalInfo.address,
+		// 		},
+		// 	});
+		// };
 	}
 
 	render() {
-		return <div>Main</div>;
+		return (
+			<div className="p-10 bg-gray-300 font-sans flex justify-center">
+				<CVForm />
+			</div>
+		);
 	}
 }
