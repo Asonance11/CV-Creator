b1e7bf67b80fa96e840df196429d1b27de79e8c8
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-18 13:23:45 +0100

    adddition of button to convert the preview to pdf

--- src/components/CV-Form/CVForm.jsx
+++ src/components/CV-Form/CVForm.jsx
@@ -1,3 +1,4 @@
+import html2pdf from 'html2pdf.js';
 import React from 'react';
 import Education from './Education';
 import Experience from './Experience';
@@ -5,6 +6,18 @@ import PersonalInfo from './PersonalInfo';
 import Skills from './Skills';
 
 const CVForm = (props) => {
+	function printPreview() {
+		const element = document.getElementById('cv-preview');
+		let opt = {
+			margin: 1,
+			filename: 'myCV.pdf',
+			image: { type: 'jpeg', quality: 0.98 },
+			html2canvas: { scale: 2 },
+			jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+		};
+		html2pdf().set(opt).from(element).save();
+	}
+
 	return (
 		<form className="mb-8 bg-slate-100 p-4 rounded-lg shadow-2xl w-[350px] sm:w-[80vw]">
 			<PersonalInfo
@@ -29,6 +42,14 @@ const CVForm = (props) => {
 				addSkill={props.addSkill}
 				deleteSkill={props.deleteSkill}
 			/>
+
+			<button
+				type="button"
+				className="text-white p-4 bg-red-600 rounded-md w-full"
+				onClick={printPreview}
+			>
+				Convert to PDF
+			</button>
 		</form>
 	);
 };
