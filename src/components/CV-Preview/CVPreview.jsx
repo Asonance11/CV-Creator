2e1d7daac9bc008032e47694041d4dcb812d9569
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-18 10:56:44 +0100

    creation of CV preview child components

---
+++ src/components/CV-Preview/CVPreview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CVPreview = (props) => {
+	return (
+		<section id="cv-preview" className="mb-8">
+			CV-Preview
+		</section>
+	);
+};
+
+export default CVPreview;
