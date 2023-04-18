50f86e52776e56b3915b9e459dc7ad60bb467fdc
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-18 12:40:27 +0100

    skills preview component

--- src/components/CV-Preview/Skills.jsx
+++ src/components/CV-Preview/Skills.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Skills = () => {
-	return <div>Skills</div>;
+const Skills = (props) => {
+	const allSkills = props.skills.map((skill, index) => (
+		<li key={index + 10}>{skill[index]}</li>
+	));
+
+	return (
+		<section>
+			<h2>Skills</h2>
+			{allSkills}
+		</section>
+	);
 };
 
 export default Skills;
