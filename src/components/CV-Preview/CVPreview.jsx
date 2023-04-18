89b6095b33f506308f3e4d7b9aa2971594f5853d
Oluwadabira <adigunsamuelaso@gmail.com>
2023-04-18 11:02:08 +0100

    addition of props to Cv preview children components

--- src/components/CV-Preview/CVPreview.jsx
+++ src/components/CV-Preview/CVPreview.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Education from './Education';
+import Experience from './Experience';
+import Personal from './Personal';
+import Skills from './Skills';
 
 const CVPreview = (props) => {
 	return (
-		<section id="cv-preview" className="mb-8">
-			CV-Preview
+		<section
+			id="cv-preview"
+			className="mb-8 bg-white p-4  shadow-2xl w-[350px] sm:w-[80vw]"
+		>
+			<Personal infomation={props.personalInfo} />
+			<Experience experience={props.experience} />
+			<Education education={props.education} />
+			<Skills skills={props.skills} />
 		</section>
 	);
 };
