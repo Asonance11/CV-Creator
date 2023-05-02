57d49b12224c4f2615896f1e6cf609166158b17d
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-02 10:18:03 +0100

    refactor: Change to functional component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,83 +1,67 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import CVForm from './CV-Form/CVForm';
 import CVPreview from './CV-Preview/CVPreview';
 
-export default class Main extends Component {
-	constructor() {
-		super();
+const Main = () => {
+	const [personalInfo, setPersonalInfo] = useState({
+		firstName: '',
+		lastName: '',
+		jobTitle: '',
+		email: '',
+		phoneNumber: '',
+		address: '',
+	});
+	const [experience, setExperience] = useState([
+		{
+			position: '',
+			company: '',
+			startDate: '',
+			endDate: '',
+			description: '',
+		},
+	]);
+	const [education, setEducation] = useState([
+		{
+			school: '',
+			course: '',
+			startDate: '',
+			endDate: '',
+		},
+	]);
+	const [skills, setSkills] = useState([]);
 
-		this.state = {
-			personalInfo: {
-				firstName: '',
-				lastName: '',
-				jobTitle: '',
-				email: '',
-				phoneNumber: '',
-				address: '',
-			},
-			experience: [
-				{
-					position: '',
-					company: '',
-					startDate: '',
-					endDate: '',
-					description: '',
-				},
-			],
-			education: [
-				{
-					school: '',
-					course: '',
-					startDate: '',
-					endDate: '',
-				},
-			],
-			skills: [],
-		};
-	}
-
-	updatePersonalInfo = (e) => {
+	const updatePersonalInfo = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
-		this.setState({
-			personalInfo: {
-				firstName: this.state.personalInfo.firstName,
-				lastName: this.state.personalInfo.lastName,
-				jobTitle: this.state.personalInfo.jobTitle,
-				email: this.state.personalInfo.email,
-				phoneNumber: this.state.personalInfo.phoneNumber,
-				address: this.state.personalInfo.address,
-				[name]: value,
-			},
+
+		setPersonalInfo({
+			...personalInfo,
+			[name]: value,
 		});
 	};
 
 	// Experience
-	updateExperience = (e, val) => {
+	const updateExperience = (e, val) => {
 		const name = e.target.name;
 		const value = e.target.value;
 
-		this.setState({
-			experience: this.state.experience.map((item, index) => {
+		setExperience(
+			experience.map((item, index) => {
 				if (index === val) {
 					return {
-						position: item.position,
-						company: item.company,
-						startDate: item.startDate,
-						endDate: item.endDate,
-						description: item.description,
+						...item,
 						[name]: value,
 					};
 				} else {
 					return item;
 				}
-			}),
-		});
+			})
+		);
 	};
 
-	addNewExperience = () => {
-		this.setState({
-			experience: this.state.experience.concat([
+	const addNewExperience = () => {
+		setExperience(
+			experience.concat([
 				{
 					position: '',
 					company: '',
@@ -85,101 +69,102 @@ export default class Main extends Component {
 					endDate: '',
 					description: '',
 				},
-			]),
-		});
+			])
+		);
 	};
 
-	deleteExperience = (key) => {
-		this.setState({
-			experience: this.state.experience.filter((item, index) => key !== index),
-		});
+	const deleteExperience = (key) => {
+		setExperience(experience.filter((item, index) => key !== index));
 	};
 
 	// Education
-	updateEducation = (e, val) => {
+	const updateEducation = (e, val) => {
 		const name = e.target.name;
 		const value = e.target.value;
 
-		this.setState({
-			education: this.state.education.map((item, index) => {
+		setEducation(
+			education.map((item, index) => {
 				if (index === val) {
 					return {
-						school: item.school,
-						course: item.course,
-						startDate: item.startDate,
-						endDate: item.endDate,
+						...item,
 						[name]: value,
 					};
 				} else {
 					return item;
 				}
-			}),
-		});
+			})
+		);
 	};
 
-	addNewEducation = () => {
-		this.setState({
-			education: this.state.education.concat([
+	const addNewEducation = () => {
+		setEducation(
+			education.concat([
 				{
 					school: '',
 					course: '',
 					startDate: '',
 					endDate: '',
 				},
-			]),
-		});
+			])
+		);
 	};
 
-	deleteEducation = (key) => {
-		this.setState({
-			education: this.state.education.filter((item, index) => key !== index),
-		});
+	const deleteEducation = (key) => {
+		setEducation(education.filter((item, index) => key !== index));
 	};
 
 	// skills
-	updateSkills = (e, val) => {
+	const updateSkills = (e, val) => {
 		const value = e.target.value;
-		this.setState({
-			skills: this.state.skills.map((skill, index) => {
+
+		setSkills(
+			skills.map((skill, index) => {
 				if (index === val) {
 					return value;
 				} else {
 					return skill;
 				}
-			}),
-		});
-	};
-
-	addNewSkill = () => {
-		this.setState({
-			skills: this.state.skills.concat(''),
-		});
-	};
-
-	deleteSkill = (key) => {
-		this.setState({
-			skills: this.state.skills.filter((item, index) => key !== index),
-		});
-	};
-
-	render() {
-		return (
-			<div className="p-10 bg-gray-300 font-sans flex flex-col justify-center items-center ">
-				<CVForm
-					info={this.state}
-					getPersonalInfo={this.updatePersonalInfo}
-					getExperience={this.updateExperience}
-					addExperience={this.addNewExperience}
-					deleteExperience={this.deleteExperience}
-					getEducation={this.updateEducation}
-					addEducation={this.addNewEducation}
-					deleteEducation={this.deleteEducation}
-					getSkills={this.updateSkills}
-					addSkill={this.addNewSkill}
-					deleteSkill={this.deleteSkill}
-				/>
-				<CVPreview info={this.state} />
-			</div>
+			})
 		);
-	}
-}
+	};
+
+	const addNewSkill = () => {
+		setSkills(skills.concat(''));
+	};
+
+	const deleteSKill = (key) => {
+		setSkills(
+			skills.filter((item, index) => {
+				key !== index;
+			})
+		);
+	};
+
+	let state = {
+		personalInfo: personalInfo,
+		experience: experience,
+		education: education,
+		skills: skills,
+	};
+
+	return (
+		<div className="p-10 bg-gray-300 font-sans flex flex-col justify-center items-center ">
+			<CVForm
+				info={state}
+				getPersonalInfo={updatePersonalInfo}
+				getExperience={updateExperience}
+				addExperience={addNewExperience}
+				deleteExperience={deleteExperience}
+				getEducation={updateEducation}
+				addEducation={addNewEducation}
+				deleteEducation={deleteEducation}
+				getSkills={updateSkills}
+				addSkill={addNewSkill}
+				deleteSkill={deleteSKill}
+			/>
+			<CVPreview info={state} />
+		</div>
+	);
+};
+
+export default Main;
